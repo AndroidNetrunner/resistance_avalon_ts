9bfb17d50d8ea884e6b6fccc1479e6882efa936d
AndroidNetrunner <kby9906@naver.com>
2023-02-25 02:36:44 +0900

    [FIX] 삭제 명령에서 role의 타입 체크 추가

--- src/Host.ts
+++ src/Host.ts
@@ -2,58 +2,55 @@ import { Channel, TextBasedChannel, User } from "discord.js";
 import { team, Role, roles } from "./roles";
 
 class Host {
-     userList: User[];
-    private _activeSpecialRoles: Map<string, string[]>;
-    private _channelStartedGame: TextBasedChannel;
+  userList: User[];
+  private _activeSpecialRoles: Map<string, string[]>;
+  private _channelStartedGame: TextBasedChannel;
 
-    get channelStartedGame() {
-        return this._channelStartedGame;
-    }
+  get channelStartedGame() {
+    return this._channelStartedGame;
+  }
 
-    get activeSpecialRoles() {
-        return this._activeSpecialRoles;
-    }
+  get activeSpecialRoles() {
+    return this._activeSpecialRoles;
+  }
 
-    get numberOfPlayers() {
-        return this.userList.length;
-    }
+  get numberOfPlayers() {
+    return this.userList.length;
+  }
 
-    constructor(user: User, channelStartedGame: TextBasedChannel) {
-        this.userList = [user];
-        this._channelStartedGame = channelStartedGame;
-        this._activeSpecialRoles = new Map();
-        this._activeSpecialRoles.set('loyal', [roles.Merlin]);
-        this._activeSpecialRoles.set('evil', [roles.Assassin]);
-    }
-    addNewPlayerToPlayerList(user: User) {
-        this.userList.push(user);
-    }
-    addSpecialRole(role: Role) {
-        if (!(team.loyal.includes(role) || team.evil.includes(role)))
-            return `${role}은(는) 존재하지 않는 역할입니다.`;
-        if (this._activeSpecialRoles.get('loyal')?.includes(role) || this._activeSpecialRoles.get('evil')?.includes(role))
-            return `${role}은(는) 이미 추가된 역할입니다.`;
-        if (team.loyal.includes(role))
-            this._activeSpecialRoles.get('loyal')?.push(role);
-        else
-            this._activeSpecialRoles.get('evil')?.push(role);
-        return `${role}이(가) 게임에 추가되었습니다.`;
-    }
-    removeSpecialRole(role: Role) {
-        if (!(team.loyal.includes(role) || team.evil.includes(role)))
-            return `${role}은(는) 존재하지 않는 역할입니다.`;
-        if (this._activeSpecialRoles.get('evil')?.includes(role)) {
-            const index = this._activeSpecialRoles.get('evil')?.indexOf(role);
-            this._activeSpecialRoles.get('evil')?.splice(index as number, 1);
-        }
-        else if (this._activeSpecialRoles.get('loyal')?.includes(role)) {
-            const index = this._activeSpecialRoles.get('loyal')?.indexOf(role);
-            this._activeSpecialRoles.get('loyal')?.splice(index as number, 1);
-        }
-        else
-            return `${role}은(는) 추가되지 않은 역할입니다.`;
-        return `${role}이(가) 게임에서 삭제되었습니다.`;
-    }
+  constructor(user: User, channelStartedGame: TextBasedChannel) {
+    this.userList = [user];
+    this._channelStartedGame = channelStartedGame;
+    this._activeSpecialRoles = new Map();
+    this._activeSpecialRoles.set("loyal", [roles.Merlin]);
+    this._activeSpecialRoles.set("evil", [roles.Assassin]);
+  }
+  addNewPlayerToPlayerList(user: User) {
+    this.userList.push(user);
+  }
+  addSpecialRole(role: Role) {
+    if (!(team.loyal.includes(role) || team.evil.includes(role)))
+      return `${role}은(는) 존재하지 않는 역할입니다.`;
+    if (
+      this._activeSpecialRoles.get("loyal")?.includes(role) ||
+      this._activeSpecialRoles.get("evil")?.includes(role)
+    )
+      return `${role}은(는) 이미 추가된 역할입니다.`;
+    if (team.loyal.includes(role))
+      this._activeSpecialRoles.get("loyal")?.push(role);
+    else this._activeSpecialRoles.get("evil")?.push(role);
+    return `${role}이(가) 게임에 추가되었습니다.`;
+  }
+  removeSpecialRole(role: Role) {
+    if (this._activeSpecialRoles.get("evil")?.includes(role)) {
+      const index = this._activeSpecialRoles.get("evil")?.indexOf(role);
+      this._activeSpecialRoles.get("evil")?.splice(index as number, 1);
+    } else if (this._activeSpecialRoles.get("loyal")?.includes(role)) {
+      const index = this._activeSpecialRoles.get("loyal")?.indexOf(role);
+      this._activeSpecialRoles.get("loyal")?.splice(index as number, 1);
+    } else return `${role}은(는) 추가되지 않은 역할입니다.`;
+    return `${role}이(가) 게임에서 삭제되었습니다.`;
+  }
 }
 
 export default Host;
--- src/commands/삭제.ts
+++ src/commands/삭제.ts
@@ -1,18 +1,20 @@
-import {ICommand} from 'wokcommands';
-import { active_hosts } from '../state';
-
-const removeSpecialRole : ICommand = {
-    category: 'joining',
-    description: '추가된 직업을 삭제합니다. X는 삭제할 직업을 뜻합니다. 참가를 받는 동안에만 사용할 수 있습니다. 예) /삭제 퍼시발',
-    minArgs: 1,
-    expectedArgs: '<role>',
-    callback: ({interaction, args}) => {
-        const [role] = args;
-        const currentHost = active_hosts.get(interaction.channelId);
-        if (!currentHost)
-            return '시작한 게임이 존재하지 않습니다.';
-        return currentHost.removeSpecialRole(role);
-    }
-}
+import { ICommand } from "wokcommands";
+import { active_hosts } from "../state";
+import { isValidRole } from "../roles";
+const removeSpecialRole: ICommand = {
+  category: "joining",
+  description:
+    "추가된 직업을 삭제합니다. X는 삭제할 직업을 뜻합니다. 참가를 받는 동안에만 사용할 수 있습니다. 예) /삭제 퍼시발",
+  minArgs: 1,
+  expectedArgs: "<role>",
+  callback: ({ interaction, args }) => {
+    const [role] = args;
+    if (!isValidRole(role) && role !== "멀린" && role !== "암살자")
+      return `${role}은(는) 존재하지 않는 특수역할입니다.`;
+    const currentHost = active_hosts.get(interaction.channelId);
+    if (!currentHost) return "시작한 게임이 존재하지 않습니다.";
+    return currentHost.removeSpecialRole(role);
+  },
+};
 
 export default removeSpecialRole;
